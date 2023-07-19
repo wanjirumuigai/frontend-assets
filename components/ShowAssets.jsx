0246f7c5f3f002fa79218251bcf2d3c50ec441fd
Pauline Muigai <wanjirumuigai@gmail.com>
2023-07-19 17:02:48 +0300

    added the search icon

--- components/ShowAssets.jsx
+++ components/ShowAssets.jsx
@@ -13,6 +13,7 @@ const options = {
 const ShowAssets = () => {
 
     const [assets, setAssets] = useState([])
+    const [searchTerm, setSearchTerm] = useState('')
     const router = useRouter()
 
     useEffect(() => {
@@ -67,6 +68,12 @@ function handleViewButton(id) {
     console.log(id)
 }
 
+function handleSearch(e) {
+    setSearchTerm(e.target.value)
+}
+
+console.log(searchTerm)
+
 
   return (
     <>
@@ -79,7 +86,7 @@ function handleViewButton(id) {
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
             </svg>
         </div>
-        <input  type="search" id="default-search" class="block p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required/>
+        <input  type="search" name='searchTerm' onKeyDown={handleSearch} id="default-search" value={searchTerm}  class="block p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required/>
 
     </div>
 </form>
