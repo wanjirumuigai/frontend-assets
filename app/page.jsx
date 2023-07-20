8ec6ad32e8c6101ab49d212db7ec272693b46def
Charles Swaleh <charlswall@live.com>
2023-07-20 15:14:59 +0300

    added links to each data card as a prop

--- app/page.jsx
+++ app/page.jsx
@@ -10,15 +10,15 @@ export default function Home() {
       <h1 className="main-heading text-4xl font-bold">Dashboard</h1>
       <div className="cards-container mt-5 flex flex-wrap justify-between">
         {/* Top row */}
-        <DataCard title={"Active Assets"} number={233} icon={FaBarcode} color={"rose"}/>
-        <DataCard title={"Allocated Assets"} number={200} icon={FaUserPlus} color={"lime"}/>
-        <DataCard title={"Users"} number={168} icon={FaUsers} color={"amber"}/>
-        <DataCard title={"Asset Categories"} number={6} icon={FaComputer} color={"sky"}/>
+        <DataCard title={"Active Assets"} number={233} icon={FaBarcode} color={"rose"} link={"/assets"}/>
+        <DataCard title={"Allocated Assets"} number={200} icon={FaUserPlus} color={"lime"} link={"/assets"}/>
+        <DataCard title={"Users"} number={168} icon={FaUsers} color={"amber"} link={"/users"}/>
+        <DataCard title={"Asset Categories"} number={6} icon={FaComputer} color={"sky"} link={"/assets"}/>
         {/* Bottom Row */}
-        <DataCard title={"Assets Under Repair"} number={2} icon={GiAutoRepair} color={"green"}/>
-        <DataCard title={"Disposed"} number={0} icon={ImBin} color={"orange"}/>
-        <DataCard title={"Licenses"} number={9} icon={TbLicense} color={"purple"}/>
-        <DataCard title={"Expired Licenses"} number={2} icon={TbLicenseOff} color={"red"}/>
+        <DataCard title={"Assets Under Repair"} number={2} icon={GiAutoRepair} color={"green"} link={"/assets"}/>
+        <DataCard title={"Disposed Assets"} number={0} icon={ImBin} color={"orange"} link={"/assets"}/>
+        <DataCard title={"Licenses"} number={9} icon={TbLicense} color={"purple"} link={"/licenses"}/>
+        <DataCard title={"Expired Licenses"} number={2} icon={TbLicenseOff} color={"red"} link={"/licenses"}/>
       </div>
     </>
   );
