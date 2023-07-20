580930e430ff36726380f7b29112f19410d946ea
Charles Swaleh <charlswall@live.com>
2023-07-20 13:46:24 +0300

    added different colors to the data cards

--- app/page.js
+++ app/page.js
@@ -1,13 +1,25 @@
 import DataCard from "@/components/DataCard";
-import { FaBarcode } from "react-icons/fa6";
+import { FaBarcode, FaUserPlus, FaComputer, FaUsers } from "react-icons/fa6";
+import { ImBin } from "react-icons/im";
+import { GiAutoRepair } from "react-icons/gi";
+import { TbLicense, TbLicenseOff } from "react-icons/tb";
 
 export default function Home() {
   return (
     <>
-    <h1 className="main-heading text-4xl font-bold">Dashboard</h1>
-    <div className="cards-container bg-zinc-100 mt-5">
-      <DataCard title="Active Assets" number={233} icon={FaBarcode}/>
-    </div>
+      <h1 className="main-heading text-4xl font-bold">Dashboard</h1>
+      <div className="cards-container mt-5 flex flex-wrap justify-between">
+        {/* Top row */}
+        <DataCard title={"Active Assets"} number={233} icon={FaBarcode} color={"rose"}/>
+        <DataCard title={"Allocated Assets"} number={200} icon={FaUserPlus} color={"lime"}/>
+        <DataCard title={"Users"} number={168} icon={FaUsers} color={"amber"}/>
+        <DataCard title={"Asset Categories"} number={6} icon={FaComputer} color={"sky"}/>
+        {/* Bottom Row */}
+        <DataCard title={"Assets Under Repair"} number={2} icon={GiAutoRepair} color={"green"}/>
+        <DataCard title={"Disposed"} number={0} icon={ImBin} color={"orange"}/>
+        <DataCard title={"Licenses"} number={9} icon={TbLicense} color={"purple"}/>
+        <DataCard title={"Expired Licenses"} number={2} icon={TbLicenseOff} color={"red"}/>
+      </div>
     </>
-   )
+  );
 }
--- components/DataCard.jsx
+++ components/DataCard.jsx
@@ -1,7 +1,7 @@
-export default function DataCard({title, number, icon: Component}){
+export default function DataCard({title, number, icon: Component, color}){
   return (
     <>
-      <div className="data-card bg-green-600 flex flex-row p-3 w-80 justify-around rounded-md">
+      <div className={`data-card bg-${color}-600 flex flex-row p-3 w-80 justify-around rounded-md mb-10`}>
         <div className="title-and-number flex flex-col rounded">
           <h1 className="text-5xl font-bold text-white">{number}</h1>
           <h1 className="text-lg font-bold text-white">{title}</h1>
