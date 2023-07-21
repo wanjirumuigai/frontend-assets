9bfb3e5180338c340270196c71edd82818851115
Charles Swaleh <charlswall@live.com>
2023-07-21 15:08:02 +0300

    Fixed the background color issue on the datacards

--- app/page.jsx
+++ app/page.jsx
@@ -10,15 +10,15 @@ export default function Home() {
       <h1 className="main-heading text-4xl font-bold">Dashboard</h1>
       <div className="cards-container mt-5 flex flex-wrap justify-between">
         {/* Top row */}
-        <DataCard title={"Active Assets"} number={233} icon={FaBarcode} color={"rose"} link={"/assets"}/>
-        <DataCard title={"Allocated Assets"} number={200} icon={FaUserPlus} color={"lime"} link={"/assets"}/>
-        <DataCard title={"Users"} number={168} icon={FaUsers} color={"amber"} link={"/users"}/>
-        <DataCard title={"Asset Categories"} number={6} icon={FaComputer} color={"sky"} link={"/assets"}/>
+        <DataCard title={"Active Assets"} number={233} icon={FaBarcode} cardColor="bg-rose-600" link={"/assets"}/>
+        <DataCard title={"Allocated Assets"} number={200} icon={FaUserPlus} cardColor={"bg-lime-600"} link={"/assets"}/>
+        <DataCard title={"Users"} number={168} icon={FaUsers} cardColor={"bg-amber-600"} link={"/users"}/>
+        <DataCard title={"Asset Categories"} number={6} icon={FaComputer} cardColor={"bg-sky-600"} link={"/assets"}/>
         {/* Bottom Row */}
-        <DataCard title={"Assets Under Repair"} number={2} icon={GiAutoRepair} color={"green"} link={"/assets"}/>
-        <DataCard title={"Disposed Assets"} number={0} icon={ImBin} color={"orange"} link={"/assets"}/>
-        <DataCard title={"Licenses"} number={9} icon={TbLicense} color={"purple"} link={"/licenses"}/>
-        <DataCard title={"Expired Licenses"} number={2} icon={TbLicenseOff} color={"red"} link={"/licenses"}/>
+        <DataCard title={"Assets Under Repair"} number={2} icon={GiAutoRepair} cardColor={"bg-green-600"} link={"/assets"}/>
+        <DataCard title={"Disposed Assets"} number={0} icon={ImBin} cardColor={"bg-orange-600"} link={"/assets"}/>
+        <DataCard title={"Licenses"} number={9} icon={TbLicense} cardColor={"bg-purple-600"} link={"/licenses"}/>
+        <DataCard title={"Expired Licenses"} number={2} icon={TbLicenseOff} cardColor={"bg-red-600"} link={"/licenses"}/>
       </div>
     </>
   );
--- components/DataCard.jsx
+++ components/DataCard.jsx
@@ -4,17 +4,17 @@ export default function DataCard({
   title,
   number,
   icon: Component,
-  color,
+  cardColor,
   link,
 }) {
   return (
     <Link href={link}>
       <div
-        className={`data-card bg-${color}-600 flex flex-row p-3 w-80 justify-around rounded-md mb-10`}
+        className={`${cardColor} data-card flex flex-row p-3 w-80 justify-around rounded-md mb-10`}
       >
         <div className="title-and-number flex flex-col rounded">
-          <h1 className="text-5xl font-bold text-white">{number}</h1>
-          <h1 className="text-lg font-bold text-white">{title}</h1>
+          <h1 className="text-5xl font-bold text-slate-200">{number}</h1>
+          <h1 className="text-lg font-bold text-slate-200">{title}</h1>
         </div>
         <Component size={74} />
       </div>
