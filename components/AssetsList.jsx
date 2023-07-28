9895425ab83af2bcef6cfaefa03ddb979585606f
Pauline Muigai <wanjirumuigai@gmail.com>
2023-07-28 12:20:48 +0300

    rendered assets using MUI Data Grid

--- components/AssetsList.jsx
+++ components/AssetsList.jsx
@@ -1,4 +1,3 @@
-"use client";
 import { useState, useEffect } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import Fuse from "fuse.js";
@@ -31,9 +30,30 @@ const columns = [
 
     width: 90,
   },
+  // {
+  //   field: "serialNumber",
+  //   headerName: "Serial Number",
+  //   description: "This column has a value getter and is not sortable.",
+  //   sortable: false,
+  //   width: 160,
+  //   valueGetter: (params) =>
+  //     `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+  // },
 ];
 
-export default function ShowAssets() {
+// const rows = [
+//   { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+//   { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+//   { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+//   { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+//   { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+//   { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+//   { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+//   { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+//   { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+// ];
+
+export default function AssetsList() {
   const [assets, setAssets] = useState([]);
   const [searchItems, setSearchItems] = useState([]);
   const route = useRouter();
