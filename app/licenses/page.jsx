774be4056ea927a6374a33e63752d109d19865b7
Charles Swaleh <charlswall@live.com>
2023-07-29 21:13:40 +0300

    add styling to the table

--- app/licenses/page.jsx
+++ app/licenses/page.jsx
@@ -2,11 +2,12 @@
 import { useEffect, useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
 
 export default function LicensePage() {
   const [licenses, setLicenses] = useState([]);
@@ -20,38 +21,96 @@ export default function LicensePage() {
     fetchLicenses();
   }, []);
 
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    "&:last-child td, &:last-child th": {
+      border: 0,
+    },
+  }));
+
   return (
     <>
       <h1 className="main-heading text-4xl font-bold">License Management</h1>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>License</TableCell>
-              <TableCell>Purchase Date</TableCell>
-              <TableCell>Expiry Date</TableCell>
-              <TableCell>No of Users</TableCell>
-              <TableCell>Valid For</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {licenses && licenses.map((license) => (
-              <TableRow
-                key={license.license_name}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {license.license_name}
-                </TableCell>
-                <TableCell>{license.purchase_date}</TableCell>
-                <TableCell>{license.expiry_date}</TableCell>
-                <TableCell>{license.number_of_users}</TableCell>
-                {(Math.floor((new Date(license.expiry_date) - new Date()) / 86400000)) < 0 ? <TableCell>Expired</TableCell> : <TableCell>{Math.floor((new Date(license.expiry_date) - new Date()) / 86400000)} days</TableCell>}
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <Paper sx={{ width: '75%', overflow: "auto" }} className="mt-7">
+        <TableContainer >
+          <Table aria-label="simple table">
+            <TableHead>
+              <StyledTableRow>
+                <StyledTableCell className="font-bold text-md uppercase">
+                  License
+                </StyledTableCell>
+                <StyledTableCell className="font-bold text-md uppercase">
+                  Purchase Date
+                </StyledTableCell>
+                <StyledTableCell className="font-bold text-md uppercase">
+                  Expiry Date
+                </StyledTableCell>
+                <StyledTableCell className="font-bold text-md uppercase">
+                  Valid For
+                </StyledTableCell>
+                <StyledTableCell className="font-bold text-md uppercase">
+                  No of Users
+                </StyledTableCell>
+              </StyledTableRow>
+            </TableHead>
+            <TableBody>
+              {licenses &&
+                licenses.map((license) => (
+                  <StyledTableRow
+                    key={license.license_name}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    className="font-bold text-md"
+                  >
+                    <StyledTableCell
+                      component="th"
+                      scope="row"
+                      className="font-bold text-md"
+                    >
+                      {license.license_name}
+                    </StyledTableCell>
+                    <StyledTableCell className="font-bold text-md">
+                      {license.purchase_date}
+                    </StyledTableCell>
+                    <StyledTableCell className="font-bold text-md">
+                      {license.expiry_date}
+                    </StyledTableCell>
+                    {Math.floor(
+                      (new Date(license.expiry_date) - new Date()) / 86400000
+                    ) < 0 ? (
+                      <StyledTableCell className="font-bold text-md uppercase text-red-600 bg-red-100">
+                        Expired
+                      </StyledTableCell>
+                    ) : (
+                      <StyledTableCell className="font-bold text-md uppercase text-green-500">
+                        {Math.floor(
+                          (new Date(license.expiry_date) - new Date()) /
+                            86400000
+                        )}{" "}
+                        days
+                      </StyledTableCell>
+                    )}
+                    <StyledTableCell className="font-bold text-md">
+                      {license.number_of_users}
+                    </StyledTableCell>
+                  </StyledTableRow>
+                ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
     </>
   );
 }
