fca3e465caed2885d47e59ad545df5493ba250de
Charles Swaleh <charlswall@live.com>
2023-08-06 13:59:01 +0300

    create the login component and add a conditional check for the children if a user is not authenticated

--- app/page.jsx
+++ app/page.jsx
@@ -3,12 +3,10 @@ import { useRouter } from "next/navigation";
 import Dashboard from "@/components/Dashboard";
 
 export default function Home({}) {
-  const router = useRouter()
-  const user = JSON.parse(sessionStorage.getItem("user"))
 
   return (
     <>
-      {user ? <Dashboard /> : router.push("/login")}
+      <Dashboard />
     </>
   );
 }
